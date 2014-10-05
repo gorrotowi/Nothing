29f2719d1070f16e789ec9d7458c11dfda9cac50
gorrotowi <t_o_wi@hotmail.com>
2014-10-04 20:05:55 -0500

    Cambiando nivel de API de la aplicación Agregando algunos strings BUG: App crash en Android 2.3.3 (Resuelto)

--- Android/Nothing/src/com/gorro/nothing/NothingActivity.java
+++ Android/Nothing/src/com/gorro/nothing/NothingActivity.java
@@ -1,27 +1,44 @@
 package com.gorro.nothing;
 
+import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
 import android.widget.TextView;
+import android.widget.Toast;
 
-//@SuppressLint("NewApi")
-@TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
 public class NothingActivity extends Activity {
 
-	TextView txtNothing;
+	// No, you clearly don't know who you're talking to, so let me clue you in.
+	// I am not in danger, Skyler.
+	// I AM the danger!
+	// A guy opens his door and gets shot and you think that of me? No. I am the
+	// one who knocks!
 
+	TextView txtNothing;
+	int normal = 0, largo = 0;
+	int androidVersion;
+
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_nothing);
 
-		getWindow().getDecorView().setSystemUiVisibility(
-				View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+		Log.d("This is a simple console log",
+				"well...this is the log whit nothing ;) now go to be happy to another place");
+		androidVersion = android.os.Build.VERSION.SDK_INT;
+		if (androidVersion >= android.os.Build.VERSION_CODES.HONEYCOMB_MR2) {
+			getWindow().getDecorView().setSystemUiVisibility(
+					View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+		}
 
 		Typeface tp = Typeface.createFromAsset(getAssets(),
 				"fonts/Questrial-Regular.otf");
@@ -30,14 +47,50 @@ public class NothingActivity extends Activity {
 
 		txtNothing.setTypeface(tp);
 
+		txtNothing.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				normal = normal + 1;
+				Log.e("CLick corto", normal + "");
+				if (largo == 4 && normal == 2) {
+					Toast.makeText(NothingActivity.this,
+							R.string.NothingToastStringOne, Toast.LENGTH_SHORT)
+							.show();
+					Log.e("primer cheat", "primer cheat");
+					largo = 0;
+					normal = 0;
+				}
+			}
+		});
+
+		txtNothing.setOnLongClickListener(new OnLongClickListener() {
+
+			@Override
+			public boolean onLongClick(View v) {
+				largo = largo + 1;
+				Log.e("Click largo", largo + "");
+				if (largo == 3 && normal == 5) {
+					Log.e("segundo cheat", "segundo cheat");
+					Toast.makeText(NothingActivity.this,
+							R.string.NothingToastStringTwo, Toast.LENGTH_SHORT)
+							.show();
+					largo = 0;
+					normal = 0;
+				}
+
+				return false;
+			}
+		});
+
 	}
-	
+
 	@Override
 	public void onWindowFocusChanged(boolean hasFocus) {
 		super.onWindowFocusChanged(hasFocus);
 		if (!hasFocus) {
 			Intent closeDialog = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
-            sendBroadcast(closeDialog);
+			sendBroadcast(closeDialog);
 		}
 	}
 }
