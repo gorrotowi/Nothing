046e808630d8b96f968bca50375896286c6146bc
Sierisimo Sier <sierisimo@gmail.com>
2022-01-24 16:40:16 -0600

    Upgrade of minor version of testing libs

--- Android/Nothing/app/build.gradle.kts
+++ Android/Nothing/app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
 
 repositories {
     mavenCentral()
+    google()
 }
 
 dependencies {
@@ -58,9 +59,9 @@ dependencies {
 
     testImplementation("junit:junit:4.13.2")
 
-    androidTestImplementation("androidx.test.ext:junit-ktx:1.1.2")
-    androidTestImplementation("androidx.test:core-ktx:1.3.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
+    androidTestImplementation("androidx.test.ext:junit-ktx:1.1.3")
+    androidTestImplementation("androidx.test:core-ktx:1.4.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 }
 
 detekt {
